d863404f0fb71200247db2e0bcd170be1bc799d9
Natalie Larkin <natalie.larkin@socialfinance.org.uk>
2023-02-02 15:17:33 -0800

    initial commit - code to produce sankey

---
+++ 2_QD_build_sankey.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 15:20:10 2023
+
+@author: natalie.larkin
+"""
+
+# load packages 
+import jupyter
+import pandas as pd
+import os
+import plotly 
+import plotly.graph_objects as go
+import numpy as np
+import plotly.express as px 
+
+# set file paths  
+root = "C:/Users/natalie.larkin/OneDrive - Social Finance Ltd/Desktop/Quality Data - Tool 2"
+input_loc = os.path.join(root, "Data/analysis")
+#input file 
+input_file = "sankey_input.xlsx"
+
+# load data 
+df = pd.read_excel(os.path.join(input_loc, input_file)) 
+
+# store columns as array
+source_string = df["source"].values.tolist()
+target_string = df["target"].values.tolist()
+
+
+# create index values for source and target 
+combo = source_string + target_string
+all_options = np.unique(combo)
+options_to_merge = pd.DataFrame(all_options, columns = ["options"])
+options_to_merge.reset_index(inplace=True)
+options_to_merge = options_to_merge.rename(columns = {'index':'sankey_index'})
+labels = options_to_merge["options"].values.tolist()
+
+# first merge the index values for source
+df_ind = df.merge(options_to_merge, left_on = "source", right_on = "options")
+# rename and drop so we can re-merge
+df_ind = df_ind.rename(columns = {"sankey_index":"source_index"})
+df_ind = df_ind.drop(columns = "options")
+# merge the index values for target 
+df_ind = df_ind.merge(options_to_merge, left_on = "target", right_on = "options")
+df_ind = df_ind.rename(columns = {"sankey_index":"target_index"})
+
+#turn columns into arrays so we can create a dictionary for the Sankey input
+source = df_ind["source_index"].values.tolist()
+target = df_ind["target_index"].values.tolist()
+value  = df_ind["id"].values.tolist()
+
+link = dict(source = source, target = target, value = value)
+node = dict(label = labels, pad = 15, thickness = 5)
+data = go.Sankey(link = link, node = node)
+
+fig = go.Figure(data)
+
+fig.show()
+ 
+
+
